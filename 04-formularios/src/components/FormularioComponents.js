031a35f2c2b7b36dc631743fc15ab5eeb03f1d10
Alejandro Tonatiuh <53270727+alexsarget@users.noreply.github.com>
2022-08-18 14:24:59 -0500

    Agregando mas funciones a los formularios

--- 04-formularios/src/components/FormularioComponents.js
+++ 04-formularios/src/components/FormularioComponents.js
@@ -1,22 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const FormularioComponents = () => {
+
+  const [usuario,setUsuario]=useState({});
+
+  const conseguirDatosFormulario = e=>{
+    e.preventDefault();
+
+    let datos =e.target;
+
+    let usuario= {
+      nombre:datos.nombre.value,
+      apellido:datos.apellido.value,
+      genero:datos.genero.value,
+      bio:datos.bio.value,
+      enviar:datos.enviar.value,
+    };
+    console.log(usuario);
+
+    setUsuario(usuario); 
+    
+  }
+  const cambiarDatos = e=>{
+    let name_del_inpuit =e.target.name;
+    let usuario_para_modificar=usuario;
+
+    //usuario_para_modificar[name_del_inpuit]=e.target.value;
+    setUsuario(estado_previo=>({
+        ...estado_previo,
+        [name_del_inpuit]:e.target.value
+      })
+    );
+  }
   return (
     <div>
         <h1>
             Fomularios con REACT 
         </h1>
 
-        <form onSubmit={}>
-          <input type="text" placeholder='Nombre'/>
-          <input type="text" placeholder='Apellido'/>
-          <select>
+        {usuario.enviar && 
+          (
+            <div className='info_usuario label label-gray'>
+              {usuario.nombre} {usuario.apellido} es un {usuario.genero} y su biografía es esta: <p>{usuario.bio}</p>
+            </div>
+          )
+        }
+        
+        <form onSubmit={conseguirDatosFormulario }>
+          <input type="text" placeholder='Nombre' name="nombre" onChange={cambiarDatos}/>
+          <input type="text" placeholder='Apellido' name="apellido" onChange={cambiarDatos}/>
+          <select name="genero" onChange={cambiarDatos}>
             <option value="hombre">Hombre</option>
             <option value="mujer">Mujer</option>
           </select>
-          <textarea placeholder='biografia'></textarea>
+          <textarea placeholder='biografia' name='bio' onChange={cambiarDatos}></textarea>
 
-          <input type="submit" value="Enviar"/>
+          <input type="submit" value="Enviar" name="enviar"/>
         </form>
     </div>
   )
